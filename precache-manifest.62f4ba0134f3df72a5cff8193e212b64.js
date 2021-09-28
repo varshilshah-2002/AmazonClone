b2285cb76fe2930af9e396a3dfc747d6121ac7b6
Varshil <>
2021-09-28 19:01:02 +0530

    Updates

---
+++ precache-manifest.62f4ba0134f3df72a5cff8193e212b64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff3a19aa213de31079e0804b5de507ab",
+    "url": "/AmazonClone/index.html"
+  },
+  {
+    "revision": "9048f1a99da577e2d8dc",
+    "url": "/AmazonClone/static/css/main.6095ae08.chunk.css"
+  },
+  {
+    "revision": "d8fb2922a4debcb59884",
+    "url": "/AmazonClone/static/js/2.561723a3.chunk.js"
+  },
+  {
+    "revision": "2b587c35db5df3608932eb682dfa83dc",
+    "url": "/AmazonClone/static/js/2.561723a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9048f1a99da577e2d8dc",
+    "url": "/AmazonClone/static/js/main.318963e1.chunk.js"
+  },
+  {
+    "revision": "f1ff0913ce82213eaf23",
+    "url": "/AmazonClone/static/js/runtime-main.cc192980.js"
+  }
+]);
